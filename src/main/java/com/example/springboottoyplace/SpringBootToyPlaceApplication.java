0db49eab7fbf071b5ad509599f62a89ccb038c72
ACACIANha <gkehdrn95@netand.co.kr>
2023-08-25 11:13:12 +0900

    Initial commit

---
+++ src/main/java/com/example/springboottoyplace/SpringBootToyPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottoyplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootToyPlaceApplication {
+
+	public static void main( String[] args ) {
+		SpringApplication.run( SpringBootToyPlaceApplication.class, args );
+	}
+
+}
