e774defe2f05c5eb2b68add2b71ba458dd511d7d
ACACIANha <gkehdrn95@netand.co.kr>
2023-08-25 11:15:40 +0900

    Initial commit

---
+++ JsonColumnMapping/src/main/java/com/example/jsoncolumnmapping/JsonColumnMappingApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsoncolumnmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonColumnMappingApplication {
+
+	public static void main( String[] args ) {
+		SpringApplication.run( JsonColumnMappingApplication.class, args );
+	}
+
+}
---
+++ JsonColumnMapping/src/test/java/com/example/jsoncolumnmapping/JsonColumnMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsoncolumnmapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonColumnMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
