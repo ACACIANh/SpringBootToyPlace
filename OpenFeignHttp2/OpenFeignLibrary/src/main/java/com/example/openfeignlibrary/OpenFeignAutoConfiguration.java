fe0fcb7f3336867998286005f2619b34c8b5ba28
ACACIANha <gkehdrn95@netand.co.kr>
2023-09-04 22:58:34 +0900

    OpenFeign http 2.0 원복

--- OpenFeignHttp2/OpenFeignLibrary/src/main/java/com/example/openfeignlibrary/OpenFeignAutoConfiguration.java
+++ OpenFeignHttp2/OpenFeignLibrary/src/main/java/com/example/openfeignlibrary/OpenFeignAutoConfiguration.java
@@ -1,14 +1,12 @@
 package com.example.openfeignlibrary;
 
 import okhttp3.OkHttpClient;
-import okhttp3.Protocol;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-import java.util.List;
 
 @AutoConfiguration
 @ComponentScan( basePackageClasses = OpenFeignAutoConfiguration.class )
@@ -18,9 +16,6 @@ public class OpenFeignAutoConfiguration {
 	@Bean
 	@ConditionalOnMissingBean
 	public OkHttpClient client() {
-		List< Protocol > http2 = List.of( Protocol.H2_PRIOR_KNOWLEDGE );
-		return new OkHttpClient.Builder()
-				.protocols( http2 )
-				.build();
+		return new OkHttpClient.Builder().build();
 	}
 }
